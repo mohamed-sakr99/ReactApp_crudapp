7387e184f50043806226d2d515678d8398e172e6
mohamed-sakr99 <mohsakr9191@gmail.com>
2021-04-07 14:29:26 +0200

    crudapp with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import CourseForm from './components/CourseForm';
+import CourseList from './components/CourseList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ class App extends Component {
 
-export default App;
+    state = {
+        courses :[
+            {name: 'HTML'},
+            {name: 'CSS'},
+            {name: 'JQUERY'}
+        ],
+        current:''
+    }    
+
+    //Add Course
+    updateCourse =(e)=> {
+        this.setState({
+            current: e.target.value
+        })
+    }
+    //Add Course
+    addCourse =(e)=> {
+        e.preventDefault();
+        let courses = this.state.courses;
+        let current = this.state.current;
+        courses.push({name: current});
+        this.setState({
+            courses,
+            current:''
+        })
+    }
+
+    //Delete Course
+    deleteCourse =(index)=> {
+        let courses = this.state.courses;
+        courses.splice(index, 1)
+        this.setState({
+            courses
+        })
+    }
+
+    //Edit Course
+    editCourse =(index, value)=> {
+    let courses = this.state.courses;
+    let course = courses[index];
+    course['name'] =value;
+    this.setState({
+        courses
+    })
+    }
+     render(){
+        const courses = this.state.courses;
+        const courseList = courses.map((course, index)=> {
+            return <CourseList details={course} key={index} index={index} deleteCourse={this.deleteCourse} editCourse={this.editCourse}/> 
+        })
+         return(
+             <section className="App">
+                 <h2>Add Course</h2>
+                 <CourseForm current={this.state.current} updateCourse={this.updateCourse} addCourse={this.addCourse}/>
+                 <ul>{courseList}</ul>
+             </section>
+         )
+     }
+ }
+ export default App;
